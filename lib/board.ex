7cad4c62249f51b6ecaa8b44218333073dabffae
riccjohn <riccjohn@gmail.com>
2020-01-14 13:27:33 -0600

    Add ability to add player moves Organize tests with describe block / tags

--- lib/board.ex
+++ lib/board.ex
@@ -1,7 +1,7 @@
 defmodule Board do
   def new_game do
     %{
-      :next_player => :o,
+      :next_player => :x,
       :board => %{
         {0, 0} => nil, {1, 0} =>  nil, {2, 0} => nil,
         {0, 1} => nil, {1, 1} =>  nil, {2, 1} => nil,
@@ -9,4 +9,17 @@ defmodule Board do
       }
     }
   end
+
+  def take_turn(game_state, coordinates) do
+    current_board = game_state.board
+    current_player = game_state.next_player
+
+    new_board = %{ current_board | coordinates => current_player }
+    new_player = switch_player current_player
+
+    %{ game_state | :next_player => new_player, :board => new_board }
+  end
+
+  def switch_player(:x), do: :o
+  def switch_player(:o), do: :x
 end
--- test/board_test.exs
+++ test/board_test.exs
@@ -1,33 +1,71 @@
 defmodule BoardTest do
   use ExUnit.Case
 
-  test "new_game creates data with a player and a board" do
-    game = Board.new_game
+  @moduletag :board
 
-    assert game.next_player
-    assert game.board
+  describe "new_game function" do
+    @describetag board: "new_game"
+
+    test "creates data with a player and a board" do
+      game = Board.new_game
+
+      assert game.next_player
+      assert game.board
+    end
+
+    test "creates data with the next player set to X" do
+      game = Board.new_game
+      assert game.next_player == :x
+    end
+
+    test "creates a board containing 9 elements" do
+      game = Board.new_game
+      board = game.board
+
+      assert (map_size board) == 9
+    end
+
+    test "creates a board with tuples as keys" do
+      game = Board.new_game
+      board = game.board
+      keys = Map.keys(board)
+
+      assert Enum.all?(keys, fn key -> is_tuple key end)
+    end
+
+    test "creates a board with each square set as nil" do
+      game = Board.new_game
+      board = game.board 
+
+      values = Map.values(board)
+      assert Enum.all?(values, fn value -> value == nil end)
+    end
   end
 
-  test "new_game creates a board containing 9 elements" do
-    game = Board.new_game
-    board = game.board
+  describe "take_turn function" do
+    @describetag board: "take_turn"
 
-    assert (map_size board) == 9
-  end
+    test "returns a new game state with data added to the correct square" do
+      game = Board.new_game
 
-  test "new_game creates a board with tuples as keys" do
-    game = Board.new_game
-    board = game.board
-    keys = Map.keys(board)
+      updated_board = Board.take_turn(game, {1, 1}).board
 
-    assert Enum.all?(keys, fn key -> is_tuple key end)
-  end
+      assert Map.fetch(updated_board, {1, 1}) == {:ok, :x}
+    end
 
-  test "new_game creates a board with each square set as nil" do
-    game = Board.new_game
-    board = game.board 
+    test "switches player to O after placing X's move" do
+      game = Board.new_game
+      updated_game = Board.take_turn(game, {0, 0})
 
-    values = Map.values(board)
-    assert Enum.all?(values, fn value -> value == nil end)
+      assert Map.fetch(updated_game, :next_player) == {:ok, :o}
+    end
+
+    test "switches player to X after placing O's move" do
+      new_game = Board.new_game
+      x_turn_state = Board.take_turn(new_game, {0, 0})
+      o_turn_state = Board.take_turn(x_turn_state, {1, 1})
+      
+      assert Map.fetch(o_turn_state, :next_player) == {:ok, :x}
+    end
   end
 end
