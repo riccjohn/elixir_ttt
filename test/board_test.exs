2ab76ac1a8056609ff33fd6c031781acb4353b36
riccjohn <riccjohn@gmail.com>
2020-01-15 09:17:27 -0600

    Switch order of assertions l/r

--- test/board_test.exs
+++ test/board_test.exs
@@ -15,14 +15,14 @@ defmodule BoardTest do
 
     test "creates data with the next player set to X" do
       game = Board.new_game
-      assert game.next_player == :x
+      assert :x == game.next_player
     end
 
     test "creates a board containing 9 elements" do
       game = Board.new_game
       board = game.board
 
-      assert (map_size board) == 9
+      assert 9 == (map_size board)
     end
 
     test "new_game creates a board with numbers as keys" do
@@ -37,8 +37,8 @@ defmodule BoardTest do
       game = Board.new_game
       board = game.board
       values = Map.values(board)
-  
-      assert values == Enum.to_list 0..8
+
+      assert (Enum.to_list 0..8) == values
     end
   end
 
@@ -50,14 +50,14 @@ defmodule BoardTest do
 
       updated_board = Board.take_turn(game, 4).board
 
-      assert Map.fetch(updated_board, 4) == {:ok, :x}
+      assert {:ok, :x} == Map.fetch(updated_board, 4)
     end
 
     test "switches player to O after placing X's move" do
       game = Board.new_game
       updated_game = Board.take_turn(game, 0)
 
-      assert Map.fetch(updated_game, :next_player) == {:ok, :o}
+      assert {:ok, :o} == Map.fetch(updated_game, :next_player)
     end
 
     test "switches player to X after placing O's move" do
@@ -65,7 +65,16 @@ defmodule BoardTest do
       x_turn_state = Board.take_turn(new_game, 0)
       o_turn_state = Board.take_turn(x_turn_state, 4)
       
-      assert Map.fetch(o_turn_state, :next_player) == {:ok, :x}
+      assert {:ok, :x} == Map.fetch(o_turn_state, :next_player)
+    end
+
+    test "does not allow a move if square already occupied by player input" do
+      new_game = Board.new_game
+      x_turn_state = Board.take_turn(new_game, 0)
+      o_turn_state = Board.take_turn(x_turn_state, 0)
+
+      assert {:ok, :o} == Map.fetch(o_turn_state, :next_player)
+      assert {:ok, :x} == Map.fetch(o_turn_state, 0)
     end
   end
 end
