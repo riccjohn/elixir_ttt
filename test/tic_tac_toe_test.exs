f5e2b3840885f8790fe4d03479d60ba5e17b4137
riccjohn <riccjohn@gmail.com>
2020-01-20 14:30:37 -0600

    Remove unused test for now

--- test/tic_tac_toe_test.exs
+++ test/tic_tac_toe_test.exs
@@ -4,18 +4,4 @@ defmodule TicTacToeTest do
 
   alias TicTacToe.Core.Game, as: Game
   @moduletag :TicTacToe
-
-  describe "start" do
-    @describetag TicTacToe: "start"
-
-    test "creates a new game" do
-      capture_io(fn ->
-        new_game = TicTacToe.start()
-
-        assert new_game.board
-        assert new_game.next_player
-        assert new_game.status
-      end)
-    end
-  end
 end
