172fab52cd2887d54bf0af448f1b3ffe52585257
riccjohn <riccjohn@gmail.com>
2020-01-20 14:00:17 -0600

    Add simple main fn, escript, and update gitignore

--- lib/tic_tac_toe.ex
+++ lib/tic_tac_toe.ex
@@ -2,9 +2,12 @@ defmodule TicTacToe do
   alias TicTacToe.Core.Game, as: Game
   alias TicTacToe.Console.Board, as: Board
 
-  def start do
+  def main(_) do
     game = Game.new
     Board.print(game.board)
-    game
+    move = Board.get_square_for_turn
+    updated_game = Game.take_turn(game, move)
+    Board.print(updated_game.board)
+    updated_game
   end
 end
--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule TicTacToe.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      escript: [main_module: TicTacToe],
       deps: deps()
     ]
   end
