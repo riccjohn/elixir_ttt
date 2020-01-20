9f1ace3c0ce0cf2014c9a69ec044c0f6bd76d956
riccjohn <riccjohn@gmail.com>
2020-01-20 16:18:47 -0600

    Spike for game loop

--- lib/tic_tac_toe.ex
+++ lib/tic_tac_toe.ex
@@ -8,13 +8,20 @@ defmodule TicTacToe do
     game = Game.new()
 
     print_formatted(game.board)
+    play_next_round(game, game.status)
+  end
 
+  def play_next_round(game, status) when status != {:ok} do
+    print_formatted(game.board)
+  end
+
+  def play_next_round(game, _status) do
     move = Input.get_square_for_turn(game.next_player)
     updated_game = Game.take_turn(game, move)
 
     print_formatted(updated_game.board)
-
-    updated_game
+    
+    play_next_round(updated_game, updated_game.status)
   end
 
   defp print_formatted(board) do
