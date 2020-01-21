8d176ce7402ed58cf2d69a3bed58c4759d32a6b9
riccjohn <riccjohn@gmail.com>
2020-01-21 12:04:51 -0600

    Move print dependency to TicTacToe module

--- lib/console/board.ex
+++ lib/console/board.ex
@@ -1,6 +1,5 @@
 defmodule TicTacToe.Console.Board do
   alias TicTacToe.Core.Board, as: Board
-  alias TicTacToe.Console.Output, as: Output
 
   def format(board) do
     values = [1, 2, 3, "\n", 4, 5, 6, "\n", 7, 8, 9]
@@ -16,11 +15,6 @@ defmodule TicTacToe.Console.Board do
       end
     end)
     |> Enum.join("")
-  end
-
-  def print_formatted(board) do
-    board_string = format(board)
-    Output.print(board_string)
   end
 
   defp convert_to_char(:x, _) do
--- lib/tic_tac_toe.ex
+++ lib/tic_tac_toe.ex
@@ -2,13 +2,20 @@ defmodule TicTacToe do
   alias TicTacToe.Core.Game, as: Game
   alias TicTacToe.Console.Board, as: Board
   alias TicTacToe.Console.Input, as: Input
+  alias TicTacToe.Console.Output, as: Output
 
   def main(_) do
     game = Game.new()
-    Board.print_formatted(game.board)
+
+    formatted_new_board = Board.format(game.board)
+    Output.print(formatted_new_board)
+
     move = Input.get_square_for_turn(game.next_player)
     updated_game = Game.take_turn(game, move)
-    Board.print_formatted(updated_game.board)
+
+    formatted_board = Board.format(updated_game.board)
+    Output.print(formatted_board)
+
     updated_game
   end
 end
